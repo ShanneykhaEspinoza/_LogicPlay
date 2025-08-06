139a87e82d2efcf825ea4d2eb61cfccd46566bfa
DavidTapue <dtapue15@gmail.com>
2025-08-06 12:11:44 -0600

    Programe registrar usuario, administrar usuarios ya esta boton actualizar y boton agregar, con busqueda funcionando. pd: arreglar base de datos ID

--- frmAdministrarUsuarios.Designer.vb
+++ frmAdministrarUsuarios.Designer.vb
@@ -23,7 +23,11 @@ Partial Class frmAdministrarUsuarios
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmAdministrarUsuarios))
-        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.L_ADMINUSUARIO = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.btnAgregarUsuario = New Guna.UI2.WinForms.Guna2Button()
         Me.btnEditarUsuario = New Guna.UI2.WinForms.Guna2Button()
         Me.btnVerUsuario = New Guna.UI2.WinForms.Guna2Button()
@@ -31,26 +35,45 @@ Partial Class frmAdministrarUsuarios
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Guna2Button6 = New Guna.UI2.WinForms.Guna2Button()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader4 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Guna2ContextMenuStrip1 = New Guna.UI2.WinForms.Guna2ContextMenuStrip()
+        Me.ELIMINAR_ADMINUSUARIO = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Guna2ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'ListView1
+        'L_ADMINUSUARIO
         '
-        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
-        Me.ListView1.FullRowSelect = True
-        Me.ListView1.GridLines = True
-        Me.ListView1.HideSelection = False
-        Me.ListView1.Location = New System.Drawing.Point(-4, 198)
-        Me.ListView1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
-        Me.ListView1.MultiSelect = False
-        Me.ListView1.Name = "ListView1"
-        Me.ListView1.Size = New System.Drawing.Size(912, 312)
-        Me.ListView1.TabIndex = 5
-        Me.ListView1.UseCompatibleStateImageBehavior = False
-        Me.ListView1.View = System.Windows.Forms.View.Details
+        Me.L_ADMINUSUARIO.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3, Me.ColumnHeader4})
+        Me.L_ADMINUSUARIO.FullRowSelect = True
+        Me.L_ADMINUSUARIO.GridLines = True
+        Me.L_ADMINUSUARIO.HideSelection = False
+        Me.L_ADMINUSUARIO.Location = New System.Drawing.Point(-4, 198)
+        Me.L_ADMINUSUARIO.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.L_ADMINUSUARIO.MultiSelect = False
+        Me.L_ADMINUSUARIO.Name = "L_ADMINUSUARIO"
+        Me.L_ADMINUSUARIO.Size = New System.Drawing.Size(912, 312)
+        Me.L_ADMINUSUARIO.TabIndex = 5
+        Me.L_ADMINUSUARIO.UseCompatibleStateImageBehavior = False
+        Me.L_ADMINUSUARIO.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "ID"
+        Me.ColumnHeader1.Width = 0
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Cedula"
+        Me.ColumnHeader2.Width = 220
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Nombre"
+        Me.ColumnHeader3.Width = 240
+        '
+        'ColumnHeader4
+        '
+        Me.ColumnHeader4.Text = "Rol"
+        Me.ColumnHeader4.Width = 220
         '
         'btnAgregarUsuario
         '
@@ -172,25 +195,28 @@ Partial Class frmAdministrarUsuarios
         Me.Label4.Text = "    Administrar usuarios"
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
-        'ColumnHeader1
+        'Guna2ContextMenuStrip1
         '
-        Me.ColumnHeader1.Text = "ID"
-        Me.ColumnHeader1.Width = 0
+        Me.Guna2ContextMenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.Guna2ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ELIMINAR_ADMINUSUARIO})
+        Me.Guna2ContextMenuStrip1.Name = "Guna2ContextMenuStrip1"
+        Me.Guna2ContextMenuStrip1.RenderStyle.ArrowColor = System.Drawing.Color.FromArgb(CType(CType(151, Byte), Integer), CType(CType(143, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2ContextMenuStrip1.RenderStyle.BorderColor = System.Drawing.Color.Gainsboro
+        Me.Guna2ContextMenuStrip1.RenderStyle.ColorTable = Nothing
+        Me.Guna2ContextMenuStrip1.RenderStyle.RoundedEdges = True
+        Me.Guna2ContextMenuStrip1.RenderStyle.SelectionArrowColor = System.Drawing.Color.White
+        Me.Guna2ContextMenuStrip1.RenderStyle.SelectionBackColor = System.Drawing.Color.FromArgb(CType(CType(100, Byte), Integer), CType(CType(88, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Guna2ContextMenuStrip1.RenderStyle.SelectionForeColor = System.Drawing.Color.White
+        Me.Guna2ContextMenuStrip1.RenderStyle.SeparatorColor = System.Drawing.Color.Gainsboro
+        Me.Guna2ContextMenuStrip1.RenderStyle.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault
+        Me.Guna2ContextMenuStrip1.Size = New System.Drawing.Size(137, 30)
         '
-        'ColumnHeader2
+        'ELIMINAR_ADMINUSUARIO
         '
-        Me.ColumnHeader2.Text = "Cedula"
-        Me.ColumnHeader2.Width = 220
-        '
-        'ColumnHeader3
-        '
-        Me.ColumnHeader3.Text = "Nombre"
-        Me.ColumnHeader3.Width = 240
-        '
-        'ColumnHeader4
-        '
-        Me.ColumnHeader4.Text = "Rol"
-        Me.ColumnHeader4.Width = 220
+        Me.ELIMINAR_ADMINUSUARIO.Image = CType(resources.GetObject("ELIMINAR_ADMINUSUARIO.Image"), System.Drawing.Image)
+        Me.ELIMINAR_ADMINUSUARIO.Name = "ELIMINAR_ADMINUSUARIO"
+        Me.ELIMINAR_ADMINUSUARIO.Size = New System.Drawing.Size(214, 26)
+        Me.ELIMINAR_ADMINUSUARIO.Text = "Eliminar"
         '
         'frmAdministrarUsuarios
         '
@@ -203,7 +229,7 @@ Partial Class frmAdministrarUsuarios
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Guna2Button6)
         Me.Controls.Add(Me.txtFiltrarParticipante)
-        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.L_ADMINUSUARIO)
         Me.Controls.Add(Me.btnAgregarUsuario)
         Me.Controls.Add(Me.btnEditarUsuario)
         Me.Controls.Add(Me.btnVerUsuario)
@@ -213,11 +239,12 @@ Partial Class frmAdministrarUsuarios
         Me.Name = "frmAdministrarUsuarios"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Form2"
+        Me.Guna2ContextMenuStrip1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents ListView1 As ListView
+    Friend WithEvents L_ADMINUSUARIO As ListView
     Friend WithEvents btnAgregarUsuario As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents btnEditarUsuario As Guna.UI2.WinForms.Guna2Button
     Friend WithEvents btnVerUsuario As Guna.UI2.WinForms.Guna2Button
@@ -229,4 +256,6 @@ Partial Class frmAdministrarUsuarios
     Friend WithEvents ColumnHeader2 As ColumnHeader
     Friend WithEvents ColumnHeader3 As ColumnHeader
     Friend WithEvents ColumnHeader4 As ColumnHeader
+    Friend WithEvents Guna2ContextMenuStrip1 As Guna.UI2.WinForms.Guna2ContextMenuStrip
+    Friend WithEvents ELIMINAR_ADMINUSUARIO As ToolStripMenuItem
 End Class
--- frmAdministrarUsuarios.vb
+++ frmAdministrarUsuarios.vb
@@ -1,5 +1,23 @@
 ﻿Public Class frmAdministrarUsuarios
+    Friend Sub BUSCANDO(ByVal CONTENIDO As String)
+        T.Tables.Clear()
+        L_ADMINUSUARIO.Items.Clear()
 
+        SQL = "SELECT ID_USUARIO, CEDULA, NOMBRE_COMPLETO, ID_ROL FROM USUARIO WHERE NOMBRE_COMPLETO LIKE '%" & CONTENIDO & "%'"
+        CARGAR_TABLA(T, SQL)
+        If T.Tables(0).Rows.Count > 0 Then
+            For I = 0 To T.Tables(0).Rows.Count - 1
+                L_ADMINUSUARIO.Items.Add(T.Tables(0).Rows(I).ItemArray(0))
+                For J = 1 To 3
+
+                    L_ADMINUSUARIO.Items(L_ADMINUSUARIO.Items.Count - 1).SubItems.Add(T.Tables(0).Rows(I).ItemArray(J))
+                Next
+            Next
+            ELIMINAR_ADMINUSUARIO.Enabled = True
+        Else
+            ELIMINAR_ADMINUSUARIO.Enabled = False
+        End If
+    End Sub
 
     Private Sub Guna2Button6_Click(sender As Object, e As EventArgs) Handles Guna2Button6.Click
         frmMenu.Show()
@@ -9,5 +27,48 @@
     Private Sub btnAgregarUsuario_Click(sender As Object, e As EventArgs) Handles btnAgregarUsuario.Click
         frmRegistro.Show()
         Me.Hide()
+    End Sub
+
+    Private Sub txtFiltrarParticipante_TextChanged(sender As Object, e As EventArgs) Handles txtFiltrarParticipante.TextChanged
+        BUSCANDO(txtFiltrarParticipante.Text)
+    End Sub
+
+    Private Sub ELIMINAR_ADMINUSUARIO_Click(sender As Object, e As EventArgs) Handles ELIMINAR_ADMINUSUARIO.Click
+        Dim ID As Integer
+        If L_ADMINUSUARIO.SelectedItems.Count > 0 Then
+            ID = L_ADMINUSUARIO.SelectedItems(0).SubItems(0).Text
+            If MsgBox("¿Desea eliminar la información seleccionada?", vbQuestion + vbYesNo, "Confirme") = vbYes Then
+                SQL = "DELETE FROM USUARIO WHERE ID = " & ID & ""
+                EJECUTAR(SQL)
+                BUSCANDO(txtFiltrarParticipante.Text)
+                MsgBox("La información ha sido eliminada satisfactoriamente.", vbInformation + vbOKOnly, "Concluido con éxito")
+            End If
+        End If
+    End Sub
+
+    Private Sub btnEditarUsuario_Click(sender As Object, e As EventArgs) Handles btnEditarUsuario.Click
+        If MsgBox("¿Desea actualizar esta información?", vbQuestion + vbYesNo, "Confirmar") = vbYes Then
+            If L_ADMINUSUARIO.SelectedItems.Count > 0 Then
+                Dim ID As Integer = L_ADMINUSUARIO.SelectedItems(0).SubItems(0).Text
+                frmRegistro.IDU_OBJ.Tag = ID
+                T.Tables.Clear()
+                SQL = "SELECT CEDULA, NOMBRE_COMPLETO, CLAVE, ID_ROL FROM USUARIO WHERE ID_USUARIO = " & ID & ""
+                CARGAR_TABLA(T, SQL)
+
+                frmRegistro.txtCedulaUsuario.Text = T.Tables(0).Rows(0).ItemArray(0)
+                frmRegistro.txtNombreUsuario.Text = T.Tables(0).Rows(0).ItemArray(1)
+                frmRegistro.txtContraseniaUsuario.Text = T.Tables(0).Rows(0).ItemArray(2)
+
+                NUEVO_USER = False 'lo que voy hacer es a modificar un usuario existente.
+                frmRegistro.Show()
+                Me.Close()
+            End If
+        Else
+            If L_ADMINUSUARIO.SelectedItems.Count > 0 Then
+                ELIMINAR_ADMINUSUARIO.Enabled = True
+            Else
+                ELIMINAR_ADMINUSUARIO.Enabled = False
+            End If
+        End If
     End Sub
 End Class
